ef344f3596ef64c0b1cc5c951359fc77094560eb
shamilysivan <140697830+shamilysivan@users.noreply.github.com>
2023-08-08 12:52:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Helo !\n'
+  const msg = '1 !\n'
   res.end(msg);
 });
 
